466f1ef355bf5017c15d007b72ac12528df1c136
demirmehmet0 <demirmehmet0@yahoo.com>
2023-04-13 17:30:29 +0300

    refactor: add widgets

--- src/Tabs.php
+++ src/Tabs.php
@@ -4,5 +4,15 @@ namespace portalium\bootstrap5;
 
 class Tabs extends Widget
 {
-    
+    public function init()
+    {
+        foreach ($this->items as $key => $item) {
+            if (!isset($item['linkOptions'])) {
+                $this->items[$key]['linkOptions'] = ['style' => 'color: #333; border-bottom: 1px solid #ddd;'];
+            }
+        }
+
+        $this->options['style'] = 'margin-bottom: 30px;';
+        parent::init();
+    }
 }
