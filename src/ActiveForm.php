bb0ae55d7bcce9df4d74ab33bef4be4bab542e38
demirmehmet0 <demirmehmet0@yahoo.com>
2024-01-20 14:04:52 +0300

    feat: add generateJsonForm

--- src/ActiveForm.php
+++ src/ActiveForm.php
@@ -4,5 +4,13 @@ namespace portalium\bootstrap5;
 
 class ActiveForm extends \yii\bootstrap5\ActiveForm
 {
+    public static function generateActiveFormWithJson($json)
+    {
+        $json = json_decode($json, true);
+        
+        $form = ActiveForm::begin($json);
 
+        return $form;
+        
+    }
 }
